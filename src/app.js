b33061aa3d29ea21b1382aeb9485087b9f593779
José Henrique A. Louteiro <78059994+henriquelouteiro@users.noreply.github.com>
2021-11-02 01:11:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const {uuid, isUuid} = require('uuidv4')
-
-// const { v4: uuid, validate: isUuid } = req('uuid');
-
 const app = express();
 
 app.use(express.json());
